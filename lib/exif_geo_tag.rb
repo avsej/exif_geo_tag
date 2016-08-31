8a2971bff7b51819a1bbf9e2b3e9037a46c0cc69
Sergey Avseyev <sergey.avseyev@gmail.com>
2016-08-31 19:28:09 +0300

    Support older MRI, which does not define negative?

--- lib/exif_geo_tag.rb
+++ lib/exif_geo_tag.rb
@@ -3,3 +3,19 @@ require 'exif_geo_tag_ext'
 
 module ExifGeoTag
 end
+
+unless 42.respond_to?(:negative?)
+  class Numeric
+    def negative?
+      self < 0
+    end
+  end
+end
+
+unless 4.2.respond_to?(:negative?)
+  class Float
+    def negative?
+      self < 0.0
+    end
+  end
+end
--- lib/exif_geo_tag/version.rb
+++ lib/exif_geo_tag/version.rb
@@ -1,3 +1,3 @@
 module ExifGeoTag
-  VERSION = "1.0.0"
+  VERSION = '1.0.1'.freeze
 end
